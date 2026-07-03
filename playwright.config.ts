f89c20bb9a6aa5caa56bc0cefb169f8ab6d7233e
joe-cole1 <joe.cole1@example.com>
2026-07-03 18:46:38 -0400

    chore:

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   webServer: {
     command: "npm run dev",
     url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
     timeout: 120_000,
   },
 });
